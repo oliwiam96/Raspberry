a1881bdde12a6dee09694371b0c27c2f8fb0118b
oliwiam96 <oliwia.m.96@wp.pl>
2018-02-10 14:45:05 +0000

    Linki pomiedzy stronami

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Oli'
+    return render_template('index.html')
+    
+
+@app.route('/halo')
+def halo():
+    return 'Mat'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name= name)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0')
+    
