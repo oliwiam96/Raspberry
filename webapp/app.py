30b6bf03b9d51e366d82c4ec5d324a1039ba26fb
oliwiam96 <oliwia.m.96@wp.pl>
2018-02-11 17:30:51 +0000

    Odczyty z bazy danych wyswietlane na stronie

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import sqlite3
+
+
+def select():
+    c.execute("SELECT * FROM pomiary")
+    data = c.fetchall()
+    print(data)
 
 app = Flask(__name__)
 @app.route('/')
@@ -8,7 +15,20 @@ def index():
 
 @app.route('/halo')
 def halo():
-    return 'Mat'
+    conn = sqlite3.connect('../pomiary.db')
+    c = conn.cursor()
+    wynik = ""
+    c.execute("SELECT * FROM pomiary ORDER BY Czas_pomiaru DESC LIMIT 10")
+    data = c.fetchall()
+    for row in data:
+        print(row)
+        wynik = wynik + str(row) + '\n' #nie dziala nowa linia i nie wiem za bardzo jak to naprawic, chyba w htmlu trzeba dodawac nowe linie za pomoca <br>
+    #print(data)
+    c.close()
+    conn.close()
+    
+    return wynik
+    #return str(data)
 
 @app.route('/hello/<name>')
 def hello(name):
